b916ef8338a4f1bd00dc254b83484313cc05d73b
Omid Moghadasi <omoghadasi@gmail.com>
2024-05-13 22:33:46 +0330

    add lsp

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,15 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
 
 		configs.setup({
-			ensure_installed = {"lua", "javascript", "html", "php"},
+			auto_install = true,
+			ensure_installed = { "lua", "javascript", "html", "php" },
 			sync_install = false,
 			highlight = { enable = true },
-			indent = { enable = true },  
+			indent = { enable = true },
 		})
-	end
+	end,
 }
-
