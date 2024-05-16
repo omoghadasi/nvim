a757044368f09fb77fc4a875b08dc178651345c3
Omid Moghadasi <omoghadasi@gmail.com>
2024-05-16 16:14:15 +0330

    add move current line to top or button

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,3 +4,5 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.g.mapleader = " "
+vim.keymap.set("n", "<A-j>", ":m +1<CR>", {})
+vim.keymap.set("n", "<A-k>", ":m -2<CR>", {})
