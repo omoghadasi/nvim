5628e12ec68a018d4d9a59431a656f753e2cd59a
omoghadasi <omoghadasi@gmail.com>
2024-05-18 12:14:05 +0330

    add venv selector plugin

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,12 +1,17 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-E>", ":Neotree filesystem reveal left toggle<CR>")
-	end,
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            enable_git_status = true,
+            enable_diagnostics = true,
+        })
+        vim.keymap.set("n", "<C-E>", ":Neotree filesystem reveal left toggle<CR>")
+    end,
 }
