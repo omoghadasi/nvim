81cd5d040126c960d29a34ea5918cfd160121cf9
Omid Moghadasi <omoghadasi@gmail.com>
2024-05-29 19:24:53 +0330

    add support persian text

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,7 @@ vim.cmd("set number")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
+vim.cmd("set arabicshape!")
 vim.g.mapleader = " "
 vim.keymap.set("n", "<A-j>", ":m +1<CR>", {})
 vim.keymap.set("n", "<A-k>", ":m -2<CR>", {})
