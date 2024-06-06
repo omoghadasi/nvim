fe40df9a962d2ca0c3ee8967ad569861e6146fc4
Omid Moghadasi <omoghadasi@gmail.com>
2024-06-06 23:16:50 +0330

    change catppuccin style

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,30 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { "italic" }, -- Change the style of comments
+				conditionals = { "italic" ,'bold'},
+				loops = {'bold'},
+				functions = { "bold" },
+				keywords = {'bold'},
+				strings = {'bold'},
+				variables = { "bold" },
+				numbers = {'bold'},
+				booleans = {'bold'},
+				properties = {'bold'},
+				types = {'bold'},
+				operators = {'bold'},
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			integrations = {
+				barbar = true,
+				mason = true,
+				diffview = true,
+				neotree = true,
+				which_key = true,
+			},
+		})
 		vim.cmd.colorscheme("catppuccin-mocha")
 	end,
 }
