2616aa80a26ef293330458b8d7f5d485c8177855
Omid Moghadasi <omoghadasi@gmail.com>
2024-06-07 14:49:54 +0330

    add telescope project

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -12,6 +12,7 @@ return {
 				null_ls.builtins.formatting.prettier,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.codespell
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, { desc = "formatting current file" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,15 +2,44 @@ return {
     {
         "nvim-telescope/telescope.nvim",
         tag = "0.1.6",
-        dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-live-grep-args.nvim", },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-live-grep-args.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
+            "nvim-telescope/telescope-project.nvim",
+        },
         config = function()
             local builtin = require("telescope.builtin")
+            local project_actions = require("telescope._extensions.project.actions")
+            require("telescope").setup({
+                extensions = {
+                    project = {
+                        hidden_files = true, -- default: false
+                        theme = "dropdown",
+                        order_by = "recent",
+                        search_by = "title",
+                        sync_with_nvim_tree = true, -- default false
+                        -- default for on_project_selected = find project files
+                        on_project_selected = function(prompt_bufnr)
+                            project_actions.change_working_directory(prompt_bufnr, false)
+                        end,
+                    },
+                },
+            })
             require("telescope").load_extension("live_grep_args")
+            require("telescope").load_extension("file_browser")
+            require("telescope").load_extension("project")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-            vim.keymap.set("n", "<leader>fs", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+            vim.keymap.set(
+                "n",
+                "<leader>fs",
+                ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
+            )
+            vim.keymap.set("n", "<leader>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+            vim.keymap.set("n", "<leader>fp", ":lua require'telescope'.extensions.project.project{}<CR>")
         end,
     },
     {
