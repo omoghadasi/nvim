ea3c875eaacd7eb8efc175fe9605a8a2eb3fea21
Omid Moghadasi <omoghadasi@gmail.com>
2024-06-23 13:17:06 +0330

    lint file

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,10 @@
 return {
-	{ "tpope/vim-fugitive" },
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({ current_line_blame = true })
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-		end,
-	},
+    { "tpope/vim-fugitive" },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup({ current_line_blame = true })
+            vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+        end,
+    },
 }
