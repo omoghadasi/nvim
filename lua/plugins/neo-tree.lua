d78dec9fa3918d0709bfe3443be451a273fb9c9a
Omid Moghadasi <omoghadasi@gmail.com>
2024-06-23 13:13:19 +0330

    change neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,11 +7,24 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        vim.fn.sign_define("DiagnosticSignError",
+            { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn",
+            { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo",
+            { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint",
+            { text = "󰌵", texthl = "DiagnosticSignHint" })
         require("neo-tree").setup({
             close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            popup_border_style = "rounded",
             enable_git_status = true,
             enable_diagnostics = true,
-            filesystem = { follow_current_file = { enabled = true } }
+            filesystem = { follow_current_file = { enabled = true } },
+            source_selector = {
+                winbar = true,
+                statusline = true
+            }
         })
         vim.keymap.set("n", "<C-E>", ":Neotree filesystem reveal left toggle<CR>")
     end,
