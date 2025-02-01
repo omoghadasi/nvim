06cfdd02e39432ed4ee41bc989755dfa07be0306
omid <omoghadasi@gmail.com>
2025-02-01 15:05:51 +0330

    add desc to keymap

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,6 +2,7 @@ return {
     "pocco81/auto-save.nvim",
     config = function()
         require("auto-save").setup()
+        vim.api.nvim_set_keymap("n", "<leader>ast", ":ASToggle<CR>", { desc = "Toggle Auto Save" })
     end
 
 }
--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 return {
-	"mbbill/undotree",
-	config = function()
-		vim.keymap.set("n", "<leader><F6>", vim.cmd.UndotreeToggle)
-	end,
+    "mbbill/undotree",
+    config = function()
+        vim.keymap.set("n", "<leader><F6>", vim.cmd.UndotreeToggle, { desc = "Undo Tree Dom Toggle" })
+    end,
 }
