dd16f8418fdfc1fb7ae9030262fde015267400b3
omid <omoghadasi@gmail.com>
2025-02-23 10:58:07 +0330

    change lua line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,8 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
+				theme = "dracula",
+				section_separators = { left = "", right = "" },
 			},
 		})
 	end,
