95776d316f687d2789d63bb022ae25031f43d36b
omid <omoghadasi@gmail.com>
2025-02-26 13:56:13 +0330

    handle fold with z key

---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,17 @@
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    config = function()
+        vim.o.foldcolumn = "1"
+        vim.o.foldlevel = 99
+        vim.o.foldlevelstart = 99
+        vim.o.foldenable = true
+        vim.o.foldmethod = "expr"
+        vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+        require("ufo").setup({
+            provider_selector = function(_, _, _)
+                return { "treesitter", "indent" }
+            end,
+        })
+    end,
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,16 +2,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        local configs = require("nvim-treesitter.configs")
-
-        configs.setup({
+        require("nvim-treesitter.configs").setup({
             auto_install = true,
             sync_install = true,
             highlight = { enable = true },
             indent = { enable = true },
-            vim.cmd("set foldmethod=expr"),
-            vim.cmd("set foldexpr=nvim_treesitter#foldexpr()"),
-            vim.cmd("set nofoldenable"),
         })
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+        vim.opt.foldenable = false
     end,
 }
