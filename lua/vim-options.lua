74bb65a7be7524ed961af62fd82ad0911cb09e9a
omoghadasi <omoghadasi@gmail.com>
2025-05-05 14:50:30 +0330

    bug fixed window in copilot

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,3 +12,4 @@ vim.keymap.set("n", "<A-k>", ":m -2<CR>", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 vim.keymap.set("i", "jj", "<Esc>", {})
+vim.opt.splitright = true
