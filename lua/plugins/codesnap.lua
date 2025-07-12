49e2c0d3f9d106a9b83e8928c67615b4e67090a3
omoghadasi <o.moghadasi@megashid.ir>
2025-07-12 11:35:20 +0330

    add codesnap plugin

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make",
+	keys = {
+		{ "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+		{ "<leader>cp", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+	},
+	opts = {
+		save_path = "~/Pictures",
+		has_breadcrumbs = true,
+		bg_theme = "grape",
+    has_line_numbers = true,
+    watermarck = "omid moghadasi"
+	},
+}
