df7dc8ffb00f326f1cba97192c3b56da848d8002
omoghadasi <o.moghadasi@megashid.ir>
2025-07-12 16:07:58 +0330

    add hardtime plugin

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+  init = function()
+    vim.keymap.set({ "n", "v" }, "<leader>th", ":Hardtime toggle<CR>", { desc = "Toggle Visual Whitespace" })
+  end,
+}
