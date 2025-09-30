dac84baaf7317c6aa8b2e0b46debd76b31350b61
omoghadasi <o.moghadasi@megashid.ir>
2025-09-30 15:19:36 +0330

    change ai plugin

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,27 @@
+return {
+	"yetone/avante.nvim",
+	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+	-- ⚠️ must add this setting! ! !
+	build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		or "make",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	---@module 'avante'
+	---@type avante.Config
+	opts = {
+		-- add any opts here
+		-- this file can contain specific instructions for your project
+		instructions_file = "avante.md",
+		-- for example
+		provider = "copilot",
+		providers = {
+			copilot = {},
+		},
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"zbirenbaum/copilot.lua",
+	},
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,71 +1,57 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "ts_ls",
-                    "bashls",
-                    "cssls",
-                    "html",
-                    "jsonls",
-                    "intelephense",
-                    "pyright",
-                    "sqlls",
-                    "tailwindcss",
-                    "lemminx",
-                    "volar"
-                },
-                automatic_setup = true,
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({ capabilities = capabilities })
-            lspconfig.pyright.setup({
-                capabilities = capabilities,
-                settings = {
-                    python = {
-                        analysis = {
-                            memoryLimit = 10240,
-                            autoSearchPaths = true, -- به طور خودکار مسیرها را جستجو می‌کند
-                            useLibraryCodeForTypes = true, -- برای استفاده از کد کتابخانه‌ها
-                            diagnosticMode = "workspace", -- خطاها را در سطح workspace بررسی می‌کند
-                            typeCheckingMode = "basic", -- سطح بررسی نوع پایه
-                        },
-                    },
-                },
-            })
-            lspconfig.ts_ls.setup({ capabilities = capabilities })
-            lspconfig.intelephense.setup({ capabilities = capabilities })
-            lspconfig.html.setup({ capabilities = capabilities })
-            lspconfig.cssls.setup({ capabilities = capabilities })
-            lspconfig.bashls.setup({ capabilities = capabilities })
-            lspconfig.jsonls.setup({ capabilities = capabilities })
-            lspconfig.sqlls.setup({ capabilities = capabilities })
-            lspconfig.tailwindcss.setup({ capabilities = capabilities })
-            lspconfig.lemminx.setup({ capabilities = capabilities })
-            lspconfig.volar.setup({
-                init_options = {
-                    vue = {
-                        hybridMode = false,
-                    },
-                },
-            })
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  -- {
+  -- 	"williamboman/mason-lspconfig.nvim",
+  -- 	config = function()
+  -- require("mason-lspconfig").setup({
+  -- 	ensure_installed = {
+  -- 		"lua_ls",
+  -- 		"ts_ls",
+  -- 		"bashls",
+  -- 		"cssls",
+  -- 		"html",
+  -- 		"jsonls",
+  -- 		"intelephense",
+  -- 		"pyright",
+  -- 		"sqlls",
+  -- 		"tailwindcss",
+  -- 		"lemminx",
+  -- 		"volar",
+  -- 	},
+  -- 	automatic_setup = true,
+  -- })
+  -- 	end,
+  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      vim.lsp.enable("lua_ls", {})
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("intelephense")
+      vim.lsp.enable("html")
+      vim.lsp.enable("cssls")
+      vim.lsp.enable("jsonls")
+      vim.lsp.enable("tailwindcss")
+      vim.lsp.enable("vue_ls")
+      vim.lsp.enable("pyright", {
+        settings = {
+          python = {
+            analysis = {
+              autoSearchPaths = true,
+              diagnosticMode = "openFilesOnly",
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
+      })
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
