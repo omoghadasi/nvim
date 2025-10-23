d1c9fb9ab1cb8d0da5eb8dbed82d683fd6081359
omoghadasi <o.moghadasi@megashid.ir>
2025-10-23 10:45:25 +0330

    change scroll

--- lua/plugins/vim-smoothie.lua
+++ lua/plugins/vim-smoothie.lua
@@ -1,3 +1,10 @@
 return {
-    'psliwka/vim-smoothie',
+	"psliwka/vim-smoothie",
+	event = "VeryLazy",
+	init = function()
+		vim.g.smoothie_speed_constant_factor = 10 -- عدد بزرگتر = سریع‌تر
+		vim.g.smoothie_speed_linear_factor = 10 -- سرعت خطی
+		vim.g.smoothie_update_interval = 20 -- میلی‌ثانیه (کمتر = روان‌تر)
+		vim.g.smoothie_enabled = 1 -- 1 = فعال، 0 = غیرفعال
+	end,
 }
